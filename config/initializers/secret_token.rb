4357c9f1e5db7a2c59251903d73a8690971cf8b7
Rolf Pedersen <rolfhsp@gmail.com>
2010-12-31 11:51:19 +0100

    Initial commit. Rails app created. Gemfile updated. Bundle installed.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Showtime::Application.config.secret_token = 'bbd86116f59af54219104d2572aae6d65b57d11f6ac35b73ee8194ffbb78886717caa753b1ad2b9d260672d0ca00369a21919cc120504a9c444724410e03d1a8'
